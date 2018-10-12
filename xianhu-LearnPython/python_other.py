6ba9c3d4dce05b03c6ad608e1df095cfed7407d0
Andy Ron <randy.njfu@gmail.com>
2018-10-12 18:31:06 +0800

    update

---
+++ xianhu-LearnPython/python_other.py
@@ -0,0 +1,27 @@
+
+False,0,'',[],{},()都可以视为假
+
+dict  {'a':1, 'b':2}
+set  {'a', 'b'}
+tuple ()
+list 
+
+
+__import__("module_name")   # 查找模块输出模块路径
+
+__all__()
+
+__class__
+
+__dict__
+
+
+
+
+chinese  汉字 拼音 繁体
+https://github.com/cleverdeng/pinyin.py
+
+国家信息
+
+ipython 中文拼音
+
